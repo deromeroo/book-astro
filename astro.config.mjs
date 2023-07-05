e31052b0cdee2939e19a7561578d9e2734822aee
Diego Romero <80493776+deromeroo@users.noreply.github.com>
2023-07-05 13:21:34 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,6 @@ export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
   site: 'https://deromeroo.github.io',
-<<<<<<< HEAD
   base: '/book-astro',
   adapter: vercel()
 });
-=======
-  base: '/book-astro',
-
-});
->>>>>>> 71a2a83d1b1a3d71b98e4cf05a72ff5bd0649592
